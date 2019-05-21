f110c4f89a38f0dd192730bfcfdae87aaae15579
Ryan Prescott <ryanprsct91@gmail.com>
2019-05-21 14:19:02 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//scroll all browser support
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        let hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
